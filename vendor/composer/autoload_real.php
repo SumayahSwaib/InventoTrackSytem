455ef0ff952efc7b3a99e985814eefbfa35d0677
SumayahSwaib <83538379+SumayahSwaib@users.noreply.github.com>
2024-01-25 17:21:12 +0300

    nnnn

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3aa05c94553a871cb1a5dbe074c0ca0b
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3aa05c94553a871cb1a5dbe074c0ca0b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3aa05c94553a871cb1a5dbe074c0ca0b', 'loadClassLoader'));
